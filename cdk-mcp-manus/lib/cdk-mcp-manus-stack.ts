09f5dd701ae92b22817a2bcb8c5f9a2f407b53fb
John Park <kyopark2014@gmail.com>
2025-05-31 15:11:51 +0900

    docker

--- cdk-mcp-manus/lib/cdk-mcp-manus-stack.ts
+++ cdk-mcp-manus/lib/cdk-mcp-manus-stack.ts
@@ -361,7 +361,7 @@ export class CdkMcpManusStack extends cdk.Stack {
     );   
 
     const ec2Policy = new iam.PolicyStatement({  
-      resources: ['arn:aws:ec2:*:*:instance/*'],
+      resources: ['*'],
       actions: ['ec2:*'],
     });
     ec2Role.attachInlinePolicy( // add bedrock policy
@@ -435,11 +435,7 @@ export class CdkMcpManusStack extends cdk.Stack {
     // S3 Bucket Access
     const s3BucketAccessPolicy = new iam.PolicyStatement({
       resources: [`*`],
-      actions: [
-        's3:ListBucket',
-        's3:ListObjectsV2',
-        's3:GetObject'
-      ],
+      actions: ['s3:*'],
     });
     ec2Role.attachInlinePolicy(
       new iam.Policy(this, `s3-bucket-access-policy-for-${projectName}`, {
@@ -691,16 +687,17 @@ export class CdkMcpManusStack extends cdk.Stack {
       'systemctl start docker',
       'systemctl enable docker',
       'usermod -aG docker ec2-user',
-      `runuser -l ec2-user -c 'cd && git clone https://github.com/kyopark2014/mcp'`,
-      `json='${JSON.stringify(environment)}' && echo "$json">/home/ec2-user/mcp/application/config.json`,
-      `runuser -l ec2-user -c 'cd mcp && docker build -t streamlit-app .'`,
+      `runuser -l ec2-user -c 'cd && git clone https://github.com/kyopark2014/${projectName}'`,
+      `json='${JSON.stringify(environment)}' && echo "$json">/home/ec2-user/${projectName}/application/config.json`,
+      `runuser -l ec2-user -c 'cd ${projectName} && docker build -t streamlit-app .'`,
       `yum install -y amazon-cloudwatch-agent`,
-      `mkdir -p /opt/aws/amazon-cloudwatch-agent/etc/`, 
-      `cp /home/ec2-user/mcp/amazon-cloudwatch-agent.json /opt/aws/amazon-cloudwatch-agent/etc/`,      
+      `mkdir -p /opt/aws/amazon-cloudwatch-agent/etc/`,
+      `cp /home/ec2-user/${projectName}/amazon-cloudwatch-agent.json /opt/aws/amazon-cloudwatch-agent/etc/`,   
+      `chmod 644 /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json`,   
       `systemctl enable amazon-cloudwatch-agent`,
       `systemctl start amazon-cloudwatch-agent`,
       `mkdir -p /etc/docker`,
-      `cp /home/ec2-user/mcp/daemon.json /etc/docker/`,
+      `cp /home/ec2-user/${projectName}/daemon.json /etc/docker/`,
       `systemctl restart docker`,
       `runuser -l ec2-user -c 'docker run -d -p 8501:8501 streamlit-app'`
     ];
